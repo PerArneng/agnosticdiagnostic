78c62e5fe352c9881d7047a4641925a6907f2bab
Per Arneng <per.arneng@scalebit.com>
2019-02-24 04:56:26 +0100

    added an initial FAKE build file

---
+++ build.fsx
@@ -0,0 +1,48 @@
+#r "paket:
+nuget Fake.DotNet.AssemblyInfoFile
+nuget Fake.IO.FileSystem
+nuget Fake.IO.Zip
+nuget Fake.Windows.Chocolatey
+nuget Fake.Tools.Git
+nuget Fake.DotNet.MSBuild prerelease
+nuget Fake.DotNet.NuGet
+nuget Fake.Core.Target //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.IO.Globbing.Operators //enables !! and globbing
+open Fake.DotNet
+open Fake.IO
+open System.IO
+open Fake.Tools.Git
+open Fake.Windows.Choco
+
+Target.create "Clean" (fun _ ->
+    !! "**/bin"
+    ++ "**/obj"
+    ++ "bin"
+    |> Shell.cleanDirs
+)
+
+// Default target
+Target.create "Build" (fun _ ->
+
+    let setParams (defaults:MSBuildParams) =
+            { defaults with
+                Verbosity = Some(Quiet)
+                Targets = ["Build"]
+                Properties =
+                    [
+                        "Optimize", "True"
+                        "DebugSymbols", "True"
+                        "Configuration", "Release"
+                    ]
+            }
+
+    MSBuild.build setParams "AgnosticDiagnostic.sln"
+)
+
+// start build
+Target.runOrDefault "Build"
